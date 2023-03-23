ac373fd6a0d65bcb94285fc66e3cad176ee8346f
Matthew Evans <git@ml-evs.science>
2023-03-23 10:43:28 +0000

    Ingest data in background

--- start_production.sh
+++ start_production.sh
@@ -7,7 +7,7 @@ touch logs/odbx_dcgat_error.log
 
 tail -f -n 20 logs/odbx_dcgat_access.log logs/odbx_dcgat_error.log &
 
-python /app/dcgat_optimade/utils.py
+python /app/dcgat_optimade/utils.py &
 
 PYTHONPATH="/app/optimade_dcgat"
 
